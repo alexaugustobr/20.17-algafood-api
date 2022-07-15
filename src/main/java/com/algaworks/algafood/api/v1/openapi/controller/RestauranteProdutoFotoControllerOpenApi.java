7fbc11c39bf09edebdfae71497f9038fbb564eda
Alex <alex.silva@algaworks.com.br>
2022-07-14 21:30:23 -0300

    dev

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@Api(tags = "Produtos")
+@Api(tags = "ProdutosFotos")
 public interface RestauranteProdutoFotoControllerOpenApi {
 
 	@ApiOperation("Atualiza a foto do produto de um restaurante")
